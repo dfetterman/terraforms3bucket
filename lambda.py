8f72713c8521f5e4f7134d5efea4a474d5417d9b
Dane Fetterman <danefett@wal3246.net.isc.upenn.edu>
2018-11-20 15:41:36 -0500

    blah

--- lambda.py
+++ lambda.py
@@ -2,22 +2,29 @@ import boto3
 
 client = boto3.client('codebuild')
 
-def start_build(projectname):
+def start_build():
     response = client.start_build(
-        projectName=projectname,
+        projectName='codebuilddltme',
         environmentVariablesOverride=[
             {
                 'name': 'Bucketname',
                 'value': 'mybucket987235aaa',
                 'type': 'PLAINTEXT'
+            },
+            {
+                'name': 'Destroy',
+                'value': 'True',
+                'type': 'PLAINTEXT'
             }
         ]
         )
+    print(response)    
     return response    
 
 
 
 def lambda_handler(event, context):
-
-    build=(start_build(codebuilddltme_us-east-1_build_Application))
-    print(build)
+    
+    start_build()
+    #build = (start_build(codebuilddltme_us-east-1_build_Application))
+    #print(build)
